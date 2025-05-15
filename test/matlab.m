7ac6c1e11bb7f467a5c9cd1c31d8d4023379b05a
Diederick Niehorster <dcnieho@gmail.com>
2025-05-16 00:00:09 +0200

    add matlab test file

---
+++ test/matlab.m
@@ -0,0 +1,30 @@
+p = fileparts(mfilename("fullpath"));
+addpath(fullfile(p,'..','matlab'))
+
+screen = ScreenConfiguration(528.0, 296.9997253417969, 1920, 1080, 650);
+
+% get all tsv files in data folder
+fold = fullfile(p,'data');
+files= dir(fold);
+files= files(~[files.isdir]);
+files= files(arrayfun(@(x) endsWith(x.name,'.tsv'),files));
+
+for f=1:length(files)
+    fprintf('----------\n%s\n',files(f).name);
+    gaze = readtable(fullfile(fold,files(f).name),'FileType','text','Delimiter','\t');
+
+    dq = compute_data_quality_from_validation(gaze, 'pixels', screen, true)     % include_data_loss for testing, this is probably *not* what you want
+
+    % and RMS S2S calculated in two ways over the whole datafile
+    dq_calc = DataQuality(gaze.left_x,gaze.left_y,gaze.timestamp/1000,'pixels',screen); % timestamps are in ms in the file
+
+    fs = str2double(files(f).name(1:end-length('Hz.tsv')));
+    window_len = round(.2*fs);  % 200 ms
+
+    fprintf('RMS S2S using median: %.4f deg\n', dq_calc.precision_RMS_S2S(@(x) median(x, 'omitnan')))
+    fprintf('RMS S2S using moving window: %.4f deg\n', dq_calc.precision_using_moving_window(window_len,"RMS_S2S"))
+
+    % data loss and effective frequency
+    fprintf('Data loss: %.1f%%\n',dq_calc.data_loss_percentage())
+    fprintf('Effective frequency: %.1f Hz\n',dq_calc.effective_frequency())
+end
