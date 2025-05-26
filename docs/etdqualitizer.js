ad194ff7dd75f43336f3cf34cc2244a00495f082
Jeroen Benjamins <j.s.benjamins@uu.nl>
2025-05-26 21:48:55 +0200

    dynamic version code, no cache, always fresh
    After loading the python package, the page dynamically shows the current version of ETDQualitizer in title of page, title on page and 'version loaded'. Meta tags added to always force no-cached current version of page.

--- docs/etdqualitizer.js
+++ docs/etdqualitizer.js
@@ -1,6 +1,6 @@
 console.log("loading pyodide...");
 async function main() {
-  let pyoide;
+
   pyodide = await loadPyodide({
     indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.3/full/",
   });
@@ -15,10 +15,18 @@ async function main() {
   try {
     await micropip.install('ETDQualitizer');
     console.log("ETDQualitizer loaded");
+    pyodide.runPython(`
+        from importlib.metadata import version
+        etdqver = version('ETDQualitizer')
+    `);
+    etdqver = pyodide.globals.get('etdqver');
+    document.title = document.title + ' v' + etdqver;
+    document.getElementById('etdqTitle').innerHTML = document.getElementById('etdqTitle').innerHTML + ' v' + etdqver;
+    document.getElementById('etdqVersion').innerHTML = 'Version loaded: ' + etdqver;
     document.getElementById('bestand').disabled = false;
     document.getElementById('lblbestand').style.backgroundColor ='cadetblue';
     document.getElementById('lblbestand').style.color ='aliceblue';
-    document.getElementById('lijst').innerHTML = document.getElementById('lijst').innerHTML + ' please select one or more files from your PyschoPy procedure, enter your<br/>setup geometry below and click Do it.'
+    document.getElementById('lijst').innerHTML = document.getElementById('lijst').innerHTML + ' please select one or more files from your PyschoPy procedure, enter your<br/>setup geometry below and click Do it.';
     document.getElementById('lblbestand').style.backgroundColor ='cadetblue';
   } catch (error) {
     document.getElementById('pyodideinfo').innerHTML = document.getElementById('pyodideinfo').innerHTML +  " and ETDQualitizer failed to install";
